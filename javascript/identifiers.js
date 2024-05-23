9ba9babfbbb961f94b6cb5ce911cc01d787a134c
gitshubhsss <shubhamranjane16@gmail.com>
2024-05-23 18:47:16 +0530

    .\identifiers.js

---
+++ javascript/identifiers.js
@@ -0,0 +1,7 @@
+let cars=["mersedies","renault duster","honda"];
+
+let deleteElement=cars.pop();
+console.log(`delete element is '${deleteElement}'`);
+cars.push("maruti suzuki");
+
+console.log(cars);
