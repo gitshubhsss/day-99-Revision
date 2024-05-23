4537ab304963e1aa2d8880ab901e7d615652f137
gitshubhsss <shubhamranjane16@gmail.com>
2024-05-23 18:49:20 +0530

    .\splice.js

---
+++ javascript/splice.js
@@ -0,0 +1,8 @@
+let colors=["yellow","blue","orange"];
+colors.splice(1,1,"mango","green");
+console.log(colors);
+
+let cars=["toyota","xuv","maruti","bmw","ferrari"];
+cars.splice(1,0,"mercedies");
+cars.splice(1,1,"wagon")
+console.log(cars);
