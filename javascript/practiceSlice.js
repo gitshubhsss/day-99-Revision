1d939746b5512f1915e48bdb26858c98e464b046
gitshubhsss <shubhamranjane16@gmail.com>
2024-05-23 18:48:41 +0530

    .\practiceSlice.js

---
+++ javascript/practiceSlice.js
@@ -0,0 +1,10 @@
+let months=["january","july","march","august"];
+months.splice(0,2,"july","june");
+// months.splice(1,0,"june");
+
+console.log(months)
+
+let language=["c","c++","html","javascript","python","java","c#","sql"];
+language.reverse();
+let index=language.indexOf("javascript");
+console.log(index);
