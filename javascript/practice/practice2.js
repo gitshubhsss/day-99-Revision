5de1e06dc43992540d0f2229419796a92c51670d
gitshubhsss <shubhamranjane16@gmail.com>
2024-05-23 19:45:05 +0530

    .\practice2.js

---
+++ javascript/practice/practice2.js
@@ -0,0 +1,10 @@
+//write a javascript program to check whether the string blank or not
+
+let str="  ";
+
+if(str.trim()!=""){
+    console.log("string is not black");
+}
+else {
+    console.log("string is black");
+}
